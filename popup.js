bcd163ef306f5775233bee78d86f493484bad8bf
Tanay Raj <tanay.raj76@gmail.com>
2021-05-14 18:09:20 +0530

    api test

--- popup.js
+++ popup.js
@@ -1,1 +1,16 @@
-var button = document.getElementById('getNoti');
+var btn = document.getElementById('getNot');
+async function getNotification() {
+  const response = await fetch(
+    'https://api.github.com/users/tanay13/received_events/public?per_page=5&page=1'
+  );
+  const result = await response.json();
+
+  result.forEach((element) => {
+    const para = document.createElement('p');
+    para.innerHTML = element.actor.avatar_url;
+    document.body.appendChild(para);
+  });
+  console.log(result);
+}
+
+btn.addEventListener('click', getNotification);
