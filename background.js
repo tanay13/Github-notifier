53bdd7dcf75f699de6a36266c669610d44d11ca7
Tanay Raj <tanay.raj76@gmail.com>
2021-05-20 21:50:28 +0530

    storage issue solve

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 async function getNotification() {
   var token = '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';
   var date = new Date();
-  date.setMinutes(date.getMinutes() - 1000);
+  date.setMinutes(date.getMinutes() - 1000000);
   const ISOFormat = date.toISOString();
   var url =
     'https://api.github.com/repos/rj200113/Test-repo/issues?page=1&per_page=5&since=' +
@@ -13,18 +13,40 @@ async function getNotification() {
   })
     .then((res) => res.json())
     .then((json) => {
-      console.log(url);
+      console.log(json);
+      var myobj = [];
       json.forEach((element) => {
-        var details = {
+        // var detail = {
+        //   title: element.title,
+        //   url: element.html_url,
+        // };
+        myobj.push({
           title: element.title,
           url: element.html_url,
-        };
-
-        localStorage.setItem(element.title, JSON.stringify(details));
+        });
+      });
+      chrome.storage.sync.set({ key: myobj }, () => {
+        console.log('saved');
       });
     });
 }
 
 getNotification();
 
-// setInterval(getNotification, 60000);
+// setInterval(getNotification, 2000);
+
+// var details = {
+//   title: 'element.titl',
+//   url: ' element.html_url',
+// };
+
+// var details1 = {
+//   title: 'dfsd',
+//   url: 'dsvcds',
+// };
+
+// var myobj = [];
+
+// myobj.push(details);
+// myobj.push(details1);
+// chrome.storage.sync.set({ key: { myobj } });
--- popup.js
+++ popup.js
@@ -1,27 +1,29 @@
-var btn = document.getElementById('getNot');
-var date = new Date();
-date.setMinutes(date.getMinutes() - 2);
-const ISOFormat = date.toISOString();
 function getNotification() {
-  for (items in localStorage) {
-    var detail = JSON.parse(localStorage.getItem(items));
-    console.log(detail);
-    const list = document.createElement('li');
-    const link = document.createElement('a');
-    link.href = detail.url;
-    link.innerHTML = detail.title;
-    list.appendChild(link);
-    document.body.appendChild(list);
-  }
+  // for (items in chrome.storage) {
+  //   // chrome.storage.sync.get(['titel', 'url'], (result) => {
+  //   //   console.log(result.title + ' ' + result.url);
+  //   //   const list = document.createElement('li');
+  //   //   const link = document.createElement('a');
+  //   //   link.href = result.url;
+  //   //   link.innerHTML = result.title;
+  //   //   list.appendChild(link);
+  //   //   document.body.appendChild(list);
+  //   // });
+  //   console.log(items);
+  // }
+  chrome.storage.sync.get('key', (result) => {
+    const arr = result.key;
 
-  // result.forEach((element) => {
-  //   const para = document.createElement('p');
-  //   para.innerHTML = element.actor.avatar_url;
-  //   document.body.appendChild(para);
-  // });
-  // console.log(result);
+    arr.forEach((element) => {
+      console.log(element.title);
+      const list = document.createElement('li');
+      const link = document.createElement('a');
+      link.href = element.url;
+      link.innerHTML = element.title;
+      list.appendChild(link);
+      document.body.appendChild(list);
+    });
+  });
 }
 
-getNotification();
-
-// btn.addEventListener('click', getNotification);
+setInterval(getNotification, 4000);
