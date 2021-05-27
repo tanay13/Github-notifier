402c0e07121cf33376dd1ae2db0be4ad500ea1e4
Tanay Raj <tanay.raj76@gmail.com>
2021-05-27 22:32:14 +0530

    custom url working

--- background.js
+++ background.js
@@ -4,48 +4,49 @@ async function getNotification() {
   date.setMinutes(date.getMinutes() - 2);
   const ISOFormat = date.toISOString();
   var repoLink;
+  var url;
   chrome.storage.sync.get(['repo'], (repo) => {
-    if (!repo.repo) repoLink = 'rj200113/Test-repo';
-    else repoLink = repo.repo;
-  });
-  var url =
-    'https://api.github.com/repos/' + repoLink + '/issues?since=' + ISOFormat;
-  fetch(url, {
-    headers: {
-      Authorization: `token ${token}`,
-    },
-  })
-    .then((res) => res.json())
-    .then((json) => {
-      if (json.length === 0) {
-        console.log('empty');
-        return;
-      }
-      var myobj = [];
-      json.forEach((element) => {
-        chrome.notifications.create(
-          '',
-          {
-            type: 'basic',
-            iconUrl: '2020-12-26.jpg',
+    repoLink = repo.repo;
+    console.log(repoLink);
+    var url =
+      'https://api.github.com/repos/' + repoLink + '/issues?since=' + ISOFormat;
+    fetch(url, {
+      headers: {
+        Authorization: `token ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.length === 0) {
+          console.log('empty');
+          return;
+        }
+        var myobj = [];
+
+        json.forEach((element) => {
+          chrome.notifications.create(
+            '',
+            {
+              type: 'basic',
+              iconUrl: '2020-12-26.jpg',
+              title: element.title,
+              message: 'new issue opened!',
+            },
+            function () {
+              console.log('Last error:', chrome.runtime.lastError);
+            }
+          );
+          myobj.push({
             title: element.title,
-            message: 'new issue opened!',
-          },
-          function () {
-            console.log('Last error:', chrome.runtime.lastError);
-          }
-        );
-        myobj.push({
-          title: element.title,
-          url: element.html_url,
+            url: element.html_url,
+          });
+        });
+
+        chrome.storage.sync.set({ key: myobj }, () => {
+          console.log('saved');
         });
       });
-      chrome.storage.sync.set({ key: myobj }, () => {
-        console.log('saved');
-      });
-
-      // console.log(json);
-    });
+  });
 }
 
 // getNotification();
